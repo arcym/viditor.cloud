ecbbd6d693d066c78f29c13ea3d06a66ffb0f721
j--a <jtangele+github@gmail.com>
2015-04-28 20:53:47 -0400

    Delete mail.keys.template.js
    template.mail.keys.js is a better name.

--- mail.keys.template.js
+++
@@ -1,8 +0,0 @@
-
-
-// Version: 15.04#01                                                      
-// This file holds variables for export to keep private from general code.
-
-exports.mailAddress   = '[EMAIL ADDRESS]';
-exports.cryptPassword = '[ENCRYPTION PASSWORD]';
-exports.hashValue     = '[ENCRYPTED EMAIL PASSWORD]';
