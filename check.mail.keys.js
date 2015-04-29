426ae1b95976559d7802e21cf8a1d65867a448d8
j--a <jtangele+github@gmail.com>
2015-04-29 00:34:43 -0400

    Update check.mail.keys.js
    changed notifyByMail to mail.notifier.process.js

--- check.mail.keys.js
+++ check.mail.keys.js
@@ -4,5 +4,5 @@
 //
 // Sends an email to verify mail.keys.js is properly formatted.
 
-var notifier = require("./notifyByMail.js");
+var notifier = require("./mail.notifier.process.js");
 notifier.notify("Viditor mail test succeeded!","Viditor will be able to notify you by email!");
